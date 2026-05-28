2e764d21468252fd6d4081401c71ec7517e1b744
Gabriel Reyes <gabriel_reyes1@icloud.com>
2026-05-28 09:03:05 +0200

    Text to image

--- PyBotEnglishApp/BotApp.py
+++ PyBotEnglishApp/BotApp.py
@@ -3,7 +3,10 @@ import pyautogui
 import keyboard
 import pyperclip
 import pytesseract
+from PIL import ImageGrab
 
+
+screenshot = ImageGrab.grabclipboard()
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -15,14 +18,17 @@ class App(customtkinter.CTk):
         self.NewDoc = customtkinter.CTkButton(self, text="Open Dictionary", command=self.open_dict)
         self.NewDoc.pack(padx=30, pady=30)
 
+
     def button_callbck(self):
         print("button clicked")
     
     def open_dict(self):
         with open("PyBotEnglishApp/Dictionary.txt", "r") as f:
             f.read(10)
+    
+    def image_to_text():
+        screenshot.save
 
-        
 
 app = App()
 app.mainloop()
