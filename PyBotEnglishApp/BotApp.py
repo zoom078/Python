512634851e1c540aa556aa367048c7bbd0c0579f
Gabriel Reyes <gabriel_reyes1@icloud.com>
2026-05-28 08:38:05 +0200

    First

---
+++ PyBotEnglishApp/BotApp.py
@@ -0,0 +1,29 @@
+import customtkinter
+import pyautogui
+import keyboard
+import pyperclip
+import pytesseract
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x150")
+
+        self.button = customtkinter.CTkButton(self, text="Start", command=self.button_callbck)
+        self.button.pack(padx=20, pady=20)
+        self.NewDoc = customtkinter.CTkButton(self, text="Open Dictionary", command=self.open_dict)
+        self.NewDoc.pack(padx=30, pady=30)
+
+    def button_callbck(self):
+        print("button clicked")
+    
+    def open_dict(self):
+        with open("PyBotEnglishApp/Dictionary.txt", "r") as f:
+            f.read(10)
+
+        
+
+app = App()
+app.mainloop()
+
