a0dc06962140462320fd3f3022f0bfffc7b1eef3
Gabriel Reyes <gabriel_reyes1@icloud.com>
2026-05-31 22:29:39 +0200

    L commit

--- PyBotEnglishApp/BotApp.py
+++ PyBotEnglishApp/BotApp.py
@@ -7,6 +7,7 @@ from pynput import mouse
 import time
 import random
 from PIL import ImageGrab
+import threading
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -18,16 +19,23 @@ class App(customtkinter.CTk):
         self.coordinates = []
         self.listener = None
         self.running = True
+        self.dict_file = "PyBotEnglishApp/Dictionary.txt"
+        self.dict_words = {}
+        self.hotkeys = {
+            'f1': self.image_to_text,
+            'f2': self.choose_cords,
+            'f3': self.stop_loop,
+        }
 
         self.button = customtkinter.CTkButton(self, text="Image to Text", command=self.image_to_text)
         self.button.pack(padx=20, pady=20)
         self.Coordinates = customtkinter.CTkButton(self, text="Choose Range", command=self.choose_cords)
         self.Coordinates.pack(padx=30, pady=30)
-        self.LoopOn = customtkinter.CTkButton(self, text="Translate", command=self.translate)
-        self.LoopOn.pack(padx=40, pady=40)
+        # self.LoopOn = customtkinter.CTkButton(self, text="Translate", command=self.translate)
+        # self.LoopOn.pack(padx=40, pady=40)
         self.LoopOff = customtkinter.CTkButton(self, text="Stop", command=self.stop_loop)
-        self.LoopOff.pack(padx=50, pady=50)
-        self.WriteDict = customtkinter.CTkButton(self, text="Write Dict", command=self.write_dict)
+        self.LoopOff.pack(padx=40, pady=40)
+        # self.WriteDict = customtkinter.CTkButton(self, text="Write Dict", command=self.write_dict)
 
     def on_click(self, x, y, button, pressed):
         print("Click twice to define screenshot coordinates")
@@ -55,29 +63,44 @@ class App(customtkinter.CTk):
         right = max(x1, x2)
         top = min(y1, y2)
         bottom = max(y1, y2)
-        
-        while self.running:
-            self.screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
-            if self.screenshot is None:
-                print("No screenshot taken")
-                return
-            text = pytesseract.image_to_string(self.screenshot)
-            print(text)
-            # with open ("PyBotEnglishApp/Dictionary.txt", "w") as f:
-            #     f.write(text)
+        # Screenshot area
+        self.screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
+        if self.screenshot is None:
+            print("No screenshot taken")
+            return
+        text = pytesseract.image_to_string(self.screenshot).strip()
+        print(text)
+        # Translate text
+        with open(self.dict_file, "r") as f:
+            for line in f:
+                words = line.strip().split()
+                if len(words) == 2:
+                    dutch, english = words
+                    self.dict_words[dutch] = english
 
-    def translate(self):
-        with open ("PyBotEnglishApp/Dictionary.txt", "r") as f:
-            text = f.read()
-            pyautogui.write(text, interval=random.uniform(0.153, 0.276))
+        pyautogui.write(text, interval=random.uniform(0.153, 0.276))
+        time.sleep(1)
+
 
     def stop_loop(self):
         print("Stopping loop...")
         self.running = False
 
     def write_dict(self):
-        with open ("PyBotEnglishApp/Dictionary.txt", "w") as f:
-            f.write("This is a test")
+        with open (self.dict_file, "w") as f:
+            for line in f:
+                words = line.strip().split()
+                if len(words) == 2:
+                    dutch, english = words
+                    self.dict_words[dutch] = english
+        print("Working")
+
+    def translation(self, word):
+        word = word.strip()
+        return self.dict_words.get(word, None)
+    
+
+
 
 
 
